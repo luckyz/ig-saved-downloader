3fb4efe56ec40e60db2eb21564ad827db26bf1e5
Lucas Zurverra <lzurverra@gmail.com>
2022-08-07 19:27:44 -0300

    refactorized version

--- main.py
+++ main.py
@@ -1,206 +1,138 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import time
-from datetime import datetime
-from instagram_private_api import Client
-from pathlib import Path
-from getpass import getpass
+from instagrapi import Client
 import os
-import re
-import logging
+from getpass import getpass
+import subprocess
+import time
 
+INSTAGRAM_USER = ""
+INSTAGRAM_PASSWORD = ""
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SETTINGS_FILE = "cookies.json"
+SETTINGS_DIR = os.path.join(BASE_DIR, SETTINGS_FILE)
 MEDIA_FOLDER = "media"
-
-BASE_DIR = Path(__file__).resolve().parent
 MEDIA_DIR = os.path.join(BASE_DIR, MEDIA_FOLDER)
 
-# Log messages to file
-logging.basicConfig(format='[%(levelname)s] - %(asctime)s: %(message)s', level=logging.INFO, filename='log.txt')  # DEBUG
+new_folders = []
+downloaded_media = {}
 
+cl = Client()
 
-def use_cookies():
-	# Bootstrap code
-	cookies = None
-	try:
-		with open("cookies.pkl", "rb") as read_cookies:
-			cookies = read_cookies.read()
-	except:
-		pass
+def use_cookies(messages=True):
+    if messages:
+        print("\n[+] Instagram Saved Downloader started...")
 
-	if cookies is None:
-		print("-> Not using cookies")
-		os.environ["INSTAGRAM_USER"] = input("-> Enter your Instagram username:\n ")
-		os.environ["INSTAGRAM_PASSWORD"] = getpass()
+    if os.path.exists(SETTINGS_DIR):
+        cl.load_settings(SETTINGS_DIR)
+        cl.login(INSTAGRAM_USER, INSTAGRAM_PASSWORD)
+        cookies = True
+    else:
+        print("-> Enter your credentials")
+        os.environ["INSTAGRAM_USER"] = input("Username: ")
+        os.environ["INSTAGRAM_PASSWORD"] = getpass()
+        cl.login(os.getenv("INSTAGRAM_USER"), os.getenv("INSTAGRAM_PASSWORD"))
+        cl.dump_settings(SETTINGS_DIR)
+        cookies = False
+    
+    if messages:
+        if cookies:
+            print("- Using cookies")
+        else:
+            print("- Not using cookies")
 
-		api = Client(os.getenv("INSTAGRAM_USER"),
-					 os.getenv("INSTAGRAM_PASSWORD"),
-					 auto_patch=True)
-	else:
-		print("-> Using cookies")
-		api = Client(os.getenv("INSTAGRAM_USER"),
-					 os.getenv("INSTAGRAM_PASSWORD"),
-					 cookie=cookies,
-					 auto_patch=True)
+def runcmd(cmd, verbose = False, *args, **kwargs):
+    process = subprocess.Popen(
+        cmd,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        text = True,
+        shell = True
+    )
 
-	cookies = api.cookie_jar.dump()
-	with open("cookies.pkl", "wb") as save_cookies:
-		save_cookies.write(cookies)
+    std_out, std_err = process.communicate()
+    if verbose:
+        print(std_out.strip(), std_err)
+    pass
 
-	return api
+def get_media_type(resource):
+    MEDIA_TYPES = {
+        1: "photo",
+        2: "video",
+        8: "album",
+    }
+    return MEDIA_TYPES[resource]
 
+def get_extension(media_type):
+    return ".png" if media_type == "photo" else ".mp4"
 
-def create_media_folder(media_folder):
-	if os.path.exists(media_folder):
-		answer = input("-> Media folder has been found. Do you want to delete \
-it with all its content? [y/(n)]: ").lower()
-		while answer not in ("yes", "y", "no", "n", ""):
-			answer = input('-> You must answer "yes" (y) or "no" (n): ').lower()
+def handle_folders(username, messages=False):
+    new_path = os.path.join(MEDIA_DIR, username)
+    if os.path.exists(MEDIA_DIR):
+        if messages:
+            print("- Media folder found\n")
+        folders_list = os.listdir(MEDIA_DIR)
+        if not username in folders_list:
+            os.mkdir(new_path)
+            print(f"- Folder created: [ {username} ]")
+            new_folders.append(username)
 
-		if answer in ("yes", "y"):
-			os.system("rm -rf %s" % media_folder)
-			print("-> Deleting media folder")
+    else:
+        os.mkdir(MEDIA_DIR)
+        os.mkdir(new_path)
 
-			os.mkdir(media_folder)
-			print("-> Media folder created")
-	else:
-		os.mkdir(media_folder)
-		print("-> Media folder created")
+    os.chdir(new_path)
+    try:
+        downloaded_media[username] += 1
+    except:
+        downloaded_media[username] = 1
 
+def download_media(media):
+    media_type = get_media_type(media.media_type)
+    if media_type == "album":
+        cl.album_download(media.pk)
+    elif media_type == "photo":
+        cl.photo_download(media.pk)
+    elif media_type == "video":
+        cl.video_download(media.pk)
 
-def ask_for_unsave():
-	answer = input("\n-> Want you unsave all saved posts? [y/(n)]: ").lower()
-	while answer not in ("yes", "y", "no", "n", ""):
-		answer = input('-> You must answer "yes" (y) or "no" (n): ').lower()
+def show_statistics():
+    def remove_chars(var):
+        return str(var).replace("[", "").replace("]", "").replace("'", "")
 
-	if answer in ("no", "n", ""):
-		answer = False
-	else:
-		answer = True
+    print(f"\n- {len(new_folders)} new folders: {remove_chars(new_folders)}")
 
-	return answer
-
-
-def current_page(page_count):
-	page_count += 1
-	print("\n-> Current page: #{0}".format(page_count + 1))
-
-	return page_count
-
-
-def select_user(username):
-	user_dir = os.path.join(MEDIA_DIR, username)
-	if not os.path.exists(user_dir):
-		os.mkdir(user_dir)
-	os.chdir(user_dir)
-
-
-def parse_results(instagram, results, page_data, unsave):
-	def detect_media_type(url):
-		match = re.findall("jpg|mp4", url)
-		return match[0]
-
-	for result in results["items"]:
-		user = result["media"]["user"]["username"]
-		id = result["media"]["id"]
-		pk = result["media"]["pk"]
-
-		if user not in page_data.keys():
-			page_data[user] = []
-
-		try:
-			carousel_imgs = result["media"]["carousel_media"]
-			# have carousel media
-			for img in carousel_imgs:
-				url = img["images"]["standard_resolution"]["url"]
-
-		except KeyError as e:
-			# dont have it
-			url = result["media"]["images"]["standard_resolution"]["url"]
-
-		finally:
-			ext = detect_media_type(url)
-			page_data[user].append((url, pk, ext))
-
-			if unsave is True:
-				instagram.unsave_photo(pk)
-
-
-	return page_data
-
-
-def accumulate(page_data, stats):
-	for user in page_data.keys():
-		count = len(page_data[user])
-		if user in stats:
-			stats[user] += count
-		else:
-			stats[user] = count
-
-	return stats
-
-
-def show_statistics(stats):
-	print("\n=== Total media files downloaded by user ===".upper())
-	for user in stats:
-		print(f"  - {user}: {stats[user]}")
-
-
-def download_media(datalist):
-	for user in datalist:
-		select_user(user)
-
-		print("-> Downloading media from user [ {} ]".format(user))
-		for number, urls in enumerate(datalist[user]):
-			url = urls[0]
-			pk = urls[1]
-			ext = urls[2]
-
-			now = datetime.utcnow()
-
-			os.system(f"wget -q -c '{url}' -O '{pk}.{ext}'")
-
+    keys = [key for key in downloaded_media.keys()]
+    values = sum([value for value in downloaded_media.values()])
+    print(f"- {values} files has been downloaded from: {remove_chars(keys)}")
 
 def main():
-	data = {}
-	stats = {}
-	page_count = 0
+    use_cookies()
 
-	instagram = use_cookies()
+    collection = cl.collection_medias("All Posts", 0)
+    username = collection[0].user.username
 
-	unsave = ask_for_unsave()
+    print(f"\n- {len(collection)} posts found")
 
-	create_media_folder(MEDIA_DIR)
+    handle_folders(username, True)
+    download_media(collection[0])
+    cl.media_unsave(collection[0].id)
 
-	results = instagram.saved_feed()
-	data = parse_results(instagram, results, data, unsave)
-	next_max_id = results.get("next_max_id")
-	stats = accumulate(data, stats)
-	print("\n-> Current page: #{}".format(page_count + 1))
+    collection.pop(0)
 
-	download_media(data)
+    count = 0
 
-	page_count = current_page(page_count)
-	time.sleep(1.5)
+    for index, media in enumerate(collection):
+        username = media.user.username
+        handle_folders(username)
+        download_media(media)
+        cl.media_unsave(media.id)
 
-	count = len(results["items"])
+        count += 1
+        if count == 21:
+            print(f"\nWaiting 5 minutes to prevent a ban\n")
+            time.sleep(60 * 5) # wait 5 minutes
 
-	while next_max_id and count <= 84:
-		results = instagram.saved_feed(max_id=next_max_id)
-		response = parse_results(instagram, results, data, unsave)
-		next_max_id = results.get("next_max_id")
-		stats = accumulate(response, stats)
-
-		download_media(data)
-
-		page_count = current_page(page_count)
-		time.sleep(1.5)
-		count += len(results["items"])
-		data = {}
-
-	show_statistics(stats)
-
-	print("\n-> All files has been succesfully downloaded!")
+    show_statistics()
 
 
-if __name__ == "__main__":
-	main()
+if "__main__" == __name__:
+    main()
